21b39999f5d578a829d73e0ee935d5047a70467b
ViktoriyaVlasova <71847067+ViktoriyaVlasova@users.noreply.github.com>
2022-11-23 10:29:18 +0300

    Add files via upload

---
+++ cinema/cinema/cinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cinemaApp.swift
+//  cinema
+//
+//  Created by Кирилл Кузнецов on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct cinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
